02c2ae7732717427579f124ed5a9cf48510b52b8
ponnanna-ks <ponnanna.ks@logycent.com>
2024-08-08 22:03:01 +0530

    Introduced persistence using local storage

--- note-app/src/context/AppContext.jsx
+++ note-app/src/context/AppContext.jsx
@@ -4,6 +4,23 @@ export const AppContext = createContext()
 
 export const AppProvider = ({children}) => {
     const [groupsData, setGroupsData] = useState([])
+    
+    useEffect(() => {
+        if(groupsData.length>0){  
+            localStorage.setItem("groupData", JSON.stringify(groupsData))
+        }
+    }, [groupsData])
+
+    useEffect(() => {
+        try{
+            let data = JSON.parse(localStorage.getItem("groupData"))
+            if(data!=null){
+                setGroupsData(data)
+            }
+        }catch(error){
+            console.error(error)
+        }
+    }, [])
 
     return(
         <AppContext.Provider
