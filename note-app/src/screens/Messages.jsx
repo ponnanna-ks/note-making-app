28878b4cd6a660a3d31cc0886274a8af45f45ad8
ponnanna-ks <ponnanna.ks@logycent.com>
2024-08-13 20:45:37 +0530

    Message items bug and commit

--- note-app/src/screens/Messages.jsx
+++ note-app/src/screens/Messages.jsx
@@ -14,8 +14,12 @@ function Messages() {
     function handleSaveClick() {
         const updatedGroups = groupsData.map(group => {
             if (group.name === selectedGroup.name) {
-                setSelectedGroup({ ...group, data: [...group.data,textAreaData] })
-                return { ...group, data: [...group.data,{message:textAreaData, timeStamp: Date.now()}]};
+                setSelectedGroup({ ...group, 
+                    data: [...group.data, {message:textAreaData, timeStamp: Date.now()}]
+                })
+                return { ...group, 
+                        data: [...group.data, {message:textAreaData, timeStamp: Date.now()}]
+                    };
             }
             return group;
         });
